890350aacc4064e85a7f2e7496817c2a91f61d70
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2012-02-01 03:41:11 +0100

    The nethttp1.rb program from Day 5, with comments.

---
+++ nethttp1.rb
@@ -0,0 +1,11 @@
+require 'net/http'
+
+# It seems we use a non canonical constructor now
+url = URI.parse('http://rubylearning.com/data/text.txt')
+
+Net::HTTP.start(url.host, url.port) do |http|
+  # We create a Net::HTTP:Get object, which seems to represent a GET request
+  req = Net::HTTP::Get.new(url.path)
+  # We run the request, get the body and print it out
+  puts http.request(req).body
+end
