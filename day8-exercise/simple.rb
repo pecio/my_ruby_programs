8ad4a09dce0bdfd669803dfb203323df1bf07638
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2012-02-16 13:01:15 +0100

    Send enconding in Content-Type.

--- day8-exercise/simple.rb
+++ day8-exercise/simple.rb
@@ -1,7 +1,7 @@
 class Simple
   def call(env)
     [200,
-     {"Content-Type" => "text/plain"},
+     {"Content-Type" => "text/plain; charset=UTF-8"},
      ["The argument supplied was \"#{ARGV[0]}\"."]
     ]
   end
