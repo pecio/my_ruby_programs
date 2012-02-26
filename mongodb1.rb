53823a57796b92e8a4a2384286ff221248d58274
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2012-02-26 13:11:24 +0100

    D13: Full MongoDB insertion/update sample.

--- mongodb1.rb
+++ mongodb1.rb
@@ -1,4 +1,9 @@
 require 'mongo'
 conn = Mongo::Connection.new
 db = conn.db("rubylearning")
-db.collection_names.each { |name| puts name }
+# db.collection_names.each { |name| puts name }
+coll = db.collection("students")
+
+doc = {:name => 'Manisha', :dob => Time.now, :loves => ['english', 'marathi'], :weight => 62, :gender => 'f', :country => 'india'}
+coll_id = coll.insert(doc)
+coll.update( { :_id => coll_id } , '$set' => { :weight => 60 } )
